d733533d84a063c056990040d0c1218d93037cb7
praful mhapsekar <prafulmhapsekar98@outlook.com>
2019-09-27 16:01:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+let products = [
+{
+    id: 1,
+    name: 'laptop'
+},
+{
+    id: 2,
+    name: 'microwave'
+}
+];
+
+let currentId = 2;
+
+let PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+app.get('/products', function(req, res) {
+    res.send({ products: products });
+});
+
+app.post('/products', function(req, res) {
+    let productName = req.body.name;
+    currentId++;
+
+    products.push({
+        id: currentId,
+        name: productName
+    });
+
+    res.send('Successfully created product!');
+});
+
+app.put('/products/:id', function(req, res) {
+    let id = req.params.id;
+    let newName = req.body.newName;
+
+    let found = false;
+
+    products.forEach(function(product, index) {
+        if (!found && product.id === Number(id)) {
+            product.name = newName;
+        }
+    });
+
+    res.send('Succesfully updated product!');
+});
+
+app.delete('/products/:id', function(req, res) {
+    let id = req.params.id;
+
+    let found = false;
+
+    products.forEach(function(product, index) {
+        if (!found && product.id === Number(id)) {
+            products.splice(index, 1);
+        }
+    });
+
+    res.send('Successfully deleted product!');
+});
+
+app.listen(PORT, function() {
+    console.log('Server listening on ' + PORT);
+});
